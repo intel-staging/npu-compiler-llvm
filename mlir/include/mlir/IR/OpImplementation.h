fd5fbf0e5348d3ce4d49fb5a221daeb52bb933d8
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2022-01-14 15:38:34 +0300

    [mlir] Relax ASM parser requirements about operands Type presence

--- mlir/include/mlir/IR/OpImplementation.h
+++ mlir/include/mlir/IR/OpImplementation.h
@@ -1654,16 +1654,22 @@ public:
                   SmallVectorImpl<Value> &result) {
     size_t operandSize = llvm::range_size(operands);
     size_t typeSize = llvm::range_size(types);
-    if (operandSize != typeSize) {
+    if (typeSize != 0 && operandSize != typeSize) {
       // If no location was provided, report errors at the beginning of the op.
       return emitError(loc.isValid() ? loc : getNameLoc())
              << "number of operands and types do not match: got " << operandSize
              << " operands and " << typeSize << " types";
     }
 
-    for (auto [operand, type] : llvm::zip_equal(operands, types))
-      if (resolveOperand(operand, type, result))
-        return failure();
+    if (typeSize == 0) {
+      for (auto it : operands)
+        if (resolveOperand(it, Type(), result))
+          return failure();
+    } else {
+      for (auto [operand, type] : llvm::zip_equal(operands, types))
+        if (resolveOperand(operand, type, result))
+          return failure();
+    }
     return success();
   }
 
