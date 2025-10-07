llvm/llvm-project

Sub-channel quantized type implementation (#120172)
This is an implementation for [RFC: Supporting Sub-Channel Quantization
in
MLIR](https://discourse.llvm.org/t/rfc-supporting-sub-channel-quantization-in-mlir/82694).

In order to make the review process easier, the PR has been divided into
the following commit labels:

1. **Add implementation for sub-channel type:** Includes the class
design for `UniformQuantizedSubChannelType`, printer/parser and bytecode
read/write support. The existing types (per-tensor and per-axis) are
unaltered.
2. **Add implementation for sub-channel type:** Lowering of
`quant.qcast` and `quant.dcast` operations to Linalg operations.
3. **Adding C/Python Apis:** We first define he C-APIs and build the
Python-APIs on top of those.
4. **Add pass to normalize generic ....:** This pass normalizes
sub-channel quantized types to per-tensor per-axis types, if possible.


A  design note:
- **Explicitly storing the `quantized_dimensions`, even when they can be
derived for ranked tensor.**
While it's possible to infer quantized dimensions from the static shape
of the scales (or zero-points) tensor for ranked
data tensors
([ref](https://discourse.llvm.org/t/rfc-supporting-sub-channel-quantization-in-mlir/82694/3)
for background), there are cases where this can lead to ambiguity and
issues with round-tripping.

```
Consider the example: tensor<2x4x!quant.uniform<i8:f32:{0:2, 0:2}, {{s00:z00, s01:z01}}>>
```

The shape of the scales tensor is [1, 2], which might suggest that only
axis 1 is quantized. While this inference is technically correct, as the
block size for axis 0 is a degenerate case (equal to the dimension
size), it can cause problems with round-tripping. Therefore, even for
ranked tensors, we are explicitly storing the quantized dimensions.
Suggestions welcome!


PS: I understand that the upcoming holidays may impact your schedule, so
please take your time with the review. There's no rush.

mlir/lib/Dialect/Quant/IR/QuantOps.cpp
@@ -17,7 +17,6 @@
 
 #include "mlir/Dialect/Quant/IR/QuantOpsDialect.cpp.inc"
 
-
 namespace mlir {
 namespace quant {
 
@@ -25,22 +24,17 @@ namespace {
 
 // Verify the integrity of per-axis quantization information, if present.
 //
-// - quantizedType
-//   Any quantized type. Any quantized type with no per-axis quantization is
-//   ignored.
+// - uniformQuantizedPerAxisType
+//   A quantized type with per-axis quantization.
 //
 // - containerType
 //   Original input or result type of the operation using the provided quantized
 //   type. Used to ensure that the quantized type appears within a tensor and
 //   that the tensor is compatible with per-axis quantization information.
 //
-LogicalResult verifyPerAxisQuantization(Operation *op,
-                                        QuantizedType quantizedType,
-                                        Type containerType) {
-  auto quantizedPerAxisType = dyn_cast<UniformQuantizedPerAxisType>(quantizedType);
-  if (!quantizedPerAxisType)
-    return success();
-
+LogicalResult verifyPerAxisQuantization(
+    Operation *op, UniformQuantizedPerAxisType uniformQuantizedPerAxisType,
+    Type containerType) {
   auto tensorType = dyn_cast<TensorType>(containerType);
   if (!tensorType)
     return op->emitError("scalar types may not use per-axis quantization");
@@ -48,15 +42,108 @@ LogicalResult verifyPerAxisQuantization(Operation *op,
   if (!tensorType.hasRank())
     return success();
 
-  int64_t quantizedDimension = quantizedPerAxisType.getQuantizedDimension();
-  if (quantizedDimension >= tensorType.getRank())
+  int32_t quantizedDimension =
+      uniformQuantizedPerAxisType.getQuantizedDimension();
+  if ((int64_t)quantizedDimension >= tensorType.getRank())
     return op->emitError("quantized dimension must be less than tensor rank");
 
   int64_t quantizedDimensionSize = tensorType.getDimSize(quantizedDimension);
   if (quantizedDimensionSize != ShapedType::kDynamic &&
-      quantizedDimensionSize != (int64_t)quantizedPerAxisType.getScales().size())
+      quantizedDimensionSize !=
+          (int64_t)uniformQuantizedPerAxisType.getScales().size())
     return op->emitError(
         "quantized dimension size does not match number of scales");
+
+  return success();
+}
+
+// Verifies that the sub-channel quantization parameters are consistent with
+// the given container type. The function checks the following:
+//
+// - The container type must be a ranked tensor type.
+// - Each quantized dimension must be less than the rank of the tensor.
+// - The size of each dimension at the quantized dimension must be divisible
+//    by the corresponding block size.
+// - The scale dimension size at each axis index should match the tensor
+//    dimension at the index divided by the corresponding block size.
+//
+// The `uniformQuantizedSubChannelType` argument provides the sub-channel
+// quantization parameters, and the `containerType` argument specifies the
+// type of the container holding the quantized data.
+//
+LogicalResult verifySubChannelQuantization(
+    Operation *op,
+    UniformQuantizedSubChannelType uniformQuantizedSubChannelType,
+    Type containerType) {
+  auto tensorType = dyn_cast<TensorType>(containerType);
+  if (!tensorType)
+    return op->emitError("scalar types may not use sub-channel quantization");
+
+  if (!tensorType.hasRank())
+    return op->emitError(
+        "tensor containing the sub-channel quantized type must be ranked");
+
+  const SmallVector<std::pair<int32_t, int64_t>> &blockSizeInfo =
+      uniformQuantizedSubChannelType.getBlockSizeInfo();
+  auto shape = tensorType.getShape();
+
+  // The dimension size of scale for an axis which is not specified as quantized
+  // dimension should be 1.
+  SmallVector<int64_t> expectedScaleShape(tensorType.getShape().size(), 1);
+  for (auto [quantizedDimension, blockSize] : blockSizeInfo) {
+    if (quantizedDimension >= tensorType.getRank())
+      return op->emitError()
+             << "quantized dimension " << quantizedDimension
+             << " must be less than tensor rank " << tensorType.getRank();
+    if (!tensorType.isDynamicDim(quantizedDimension) &&
+        tensorType.getDimSize(quantizedDimension) % blockSize != 0)
+      return op->emitError()
+             << "tensor dimension size "
+             << tensorType.getDimSize(quantizedDimension) << " at axis "
+             << quantizedDimension
+             << " must be divisible by the corresponding block size "
+             << blockSize;
+    if (tensorType.isDynamicDim(quantizedDimension))
+      expectedScaleShape[quantizedDimension] = ShapedType::kDynamic;
+    else
+      expectedScaleShape[quantizedDimension] =
+          tensorType.getDimSize(quantizedDimension) / blockSize;
+  }
+
+  // Block sizes must be greater than 0 and divide the corresponding dimension
+  // size. While a block size b must be less than or equal to the corresponding
+  // dimension size d, this constraint is implicitly enforced by requiring that
+  // d % b == 0 when d != 0.
+  //
+  // However, a problem arises when d = 0.  The divisibility constraint allows b
+  // to be any value, potentially violating the requirement that b <= d.
+  // Furthermore, if b is unspecified (implicitly equal to d), it violates the
+  // constraint that b > 0.
+  //
+  // Therefore, we explicitly disallow the case where d = 0 to maintain
+  // consistency and avoid these issues.
+  if (llvm::find(tensorType.getShape(), 0) != tensorType.getShape().end()) {
+    return op->emitError() << "tensor dimension size of zero is not allowed "
+                              "with sub-channel quantization";
+  }
+
+  auto scaleShape =
+      uniformQuantizedSubChannelType.getScales().getType().getShape();
+  if (scaleShape.size() != shape.size()) {
+    return op->emitError() << "Rank of scales " << scaleShape.size()
+                           << " must match "
+                           << "the rank of the tensor " << shape.size();
+  }
+
+  for (auto [index, scaleDim] : llvm::enumerate(expectedScaleShape)) {
+    if (expectedScaleShape[index] != ShapedType::kDynamic &&
+        expectedScaleShape[index] != scaleShape[index])
+      return op->emitError() << "dimension size " << scaleDim
+                             << " of scales tensor at axis " << index
+                             << " should match (tensor dimension at axis / "
+                                "block sizes at axis) = "
+                             << expectedScaleShape[index];
+  }
 
   return success();
 }
@@ -80,12 +167,23 @@ LogicalResult verifyQuantizationOp(Operation *op, QuantizedType quantizedType,
     return op->emitError(
         "expressed type in quantized type expected to match float type");
 
-  // Veriy integrity of per-axis quantization information, if present.
-  return verifyPerAxisQuantization(op, quantizedType, containerType);
+  // Verify integrity of per-axis quantization information, if present.
+  if (auto quantizedPerAxisType =
+          dyn_cast<UniformQuantizedPerAxisType>(quantizedType)) {
+    return verifyPerAxisQuantization(op, quantizedPerAxisType, containerType);
+  }
+
+  if (auto quantizedSubChannelType =
+          dyn_cast<UniformQuantizedSubChannelType>(quantizedType)) {
+    return verifySubChannelQuantization(op, quantizedSubChannelType,
+                                        containerType);
+  }
+
+  // At this point the type is UniformQuantizedType
+  return success();
 }
 
-}  // namespace
-
+} // namespace
 
 //===----------------------------------------------------------------------===//
 // Dialect
@@ -93,14 +191,13 @@ LogicalResult verifyQuantizationOp(Operation *op, QuantizedType quantizedType,
 
 void QuantDialect::initialize() {
   addTypes<AnyQuantizedType, CalibratedQuantizedType, UniformQuantizedType,
-           UniformQuantizedPerAxisType>();
+           UniformQuantizedPerAxisType, UniformQuantizedSubChannelType>();
   addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/Quant/IR/QuantOps.cpp.inc"
       >();
   detail::addBytecodeInterface(this);
 }
-
 
 //===----------------------------------------------------------------------===//
 // DequantizeCastOp
@@ -129,7 +226,6 @@ FloatType DequantizeCastOp::getFloatType() {
 QuantizedType DequantizeCastOp::getQuantizedType() {
   return cast<QuantizedType>(getElementTypeOrSelf(getInput().getType()));
 }
-
 
 //===----------------------------------------------------------------------===//
 // QuantizeCastOp
@@ -160,7 +256,6 @@ QuantizedType QuantizeCastOp::getQuantizedType() {
   return cast<QuantizedType>(getElementTypeOrSelf(getResult().getType()));
 }
 
-
 //===----------------------------------------------------------------------===//
 // StorageCastOp
 //===----------------------------------------------------------------------===//
@@ -175,7 +270,20 @@ LogicalResult StorageCastOp::verify() {
   // Verify integrity of per-axis quantization information, if available. While
   // the quantization type may appear in the input or the result, their tensor
   // shapes are guaranteed to be identical at this point.
-  return verifyPerAxisQuantization(*this, quantizedType, getInput().getType());
+  if (auto quantizedPerAxisType =
+          dyn_cast<UniformQuantizedPerAxisType>(quantizedType)) {
+    return verifyPerAxisQuantization(*this, quantizedPerAxisType,
+                                     getInput().getType());
+  }
+
+  if (auto quantizedSunChannelType =
+          dyn_cast<UniformQuantizedSubChannelType>(quantizedType)) {
+    return verifySubChannelQuantization(*this, quantizedSunChannelType,
+                                        getInput().getType());
+  }
+
+  // At this point the type is UniformQuantizedType
+  return success();
 }
 
 OpFoldResult StorageCastOp::fold(FoldAdaptor adaptor) {
@@ -205,10 +313,8 @@ QuantizedType StorageCastOp::getQuantizedType() {
   return cast<QuantizedType>(resultScalarType);
 }
 
-
 } // namespace quant
 } // namespace mlir
 
 #define GET_OP_CLASSES
 #include "mlir/Dialect/Quant/IR/QuantOps.cpp.inc"
-

intel-staging/npu-compiler-llvm

mlir/lib/Dialect/Quant/IR/QuantOps.cpp
@@ -25,22 +25,17 @@ namespace {
 
 // Verify the integrity of per-axis quantization information, if present.
 //
-// - quantizedType
-//   Any quantized type. Any quantized type with no per-axis quantization is
-//   ignored.
+// - uniformQuantizedPerAxisType
+//   A quantized type with per-axis quantization.
 //
 // - containerType
 //   Original input or result type of the operation using the provided quantized
 //   type. Used to ensure that the quantized type appears within a tensor and
 //   that the tensor is compatible with per-axis quantization information.
 //
-LogicalResult verifyPerAxisQuantization(Operation *op,
-                                        QuantizedType quantizedType,
-                                        Type containerType) {
-  auto quantizedPerAxisType = dyn_cast<UniformQuantizedPerAxisType>(quantizedType);
-  if (!quantizedPerAxisType)
-    return success();
-
+LogicalResult verifyPerAxisQuantization(
+    Operation *op, UniformQuantizedPerAxisType uniformQuantizedPerAxisType,
+    Type containerType) {
   auto tensorType = dyn_cast<TensorType>(containerType);
   if (!tensorType)
     return op->emitError("scalar types may not use per-axis quantization");
@@ -48,15 +43,108 @@ LogicalResult verifyPerAxisQuantization(Operation *op,
   if (!tensorType.hasRank())
     return success();
 
-  int64_t quantizedDimension = quantizedPerAxisType.getQuantizedDimension();
-  if (quantizedDimension >= tensorType.getRank())
+  int32_t quantizedDimension =
+      uniformQuantizedPerAxisType.getQuantizedDimension();
+  if ((int64_t)quantizedDimension >= tensorType.getRank())
     return op->emitError("quantized dimension must be less than tensor rank");
 
   int64_t quantizedDimensionSize = tensorType.getDimSize(quantizedDimension);
   if (quantizedDimensionSize != ShapedType::kDynamic &&
-      quantizedDimensionSize != (int64_t)quantizedPerAxisType.getScales().size())
+      quantizedDimensionSize !=
+          (int64_t)uniformQuantizedPerAxisType.getScales().size())
     return op->emitError(
         "quantized dimension size does not match number of scales");
+
+  return success();
+}
+
+// Verifies that the sub-channel quantization parameters are consistent with
+// the given container type. The function checks the following:
+//
+// - The container type must be a ranked tensor type.
+// - Each quantized dimension must be less than the rank of the tensor.
+// - The size of each dimension at the quantized dimension must be divisible
+//    by the corresponding block size.
+// - The scale dimension size at each axis index should match the tensor
+//    dimension at the index divided by the corresponding block size.
+//
+// The `uniformQuantizedSubChannelType` argument provides the sub-channel
+// quantization parameters, and the `containerType` argument specifies the
+// type of the container holding the quantized data.
+//
+LogicalResult verifySubChannelQuantization(
+    Operation *op,
+    UniformQuantizedSubChannelType uniformQuantizedSubChannelType,
+    Type containerType) {
+  auto tensorType = dyn_cast<TensorType>(containerType);
+  if (!tensorType)
+    return op->emitError("scalar types may not use sub-channel quantization");
+
+  if (!tensorType.hasRank())
+    return op->emitError(
+        "tensor containing the sub-channel quantized type must be ranked");
+
+  const SmallVector<std::pair<int32_t, int64_t>> &blockSizeInfo =
+      uniformQuantizedSubChannelType.getBlockSizeInfo();
+  auto shape = tensorType.getShape();
+
+  // The dimension size of scale for an axis which is not specified as quantized
+  // dimension should be 1.
+  SmallVector<int64_t> expectedScaleShape(tensorType.getShape().size(), 1);
+  for (auto [quantizedDimension, blockSize] : blockSizeInfo) {
+    if (quantizedDimension >= tensorType.getRank())
+      return op->emitError()
+             << "quantized dimension " << quantizedDimension
+             << " must be less than tensor rank " << tensorType.getRank();
+    if (!tensorType.isDynamicDim(quantizedDimension) &&
+        tensorType.getDimSize(quantizedDimension) % blockSize != 0)
+      return op->emitError()
+             << "tensor dimension size "
+             << tensorType.getDimSize(quantizedDimension) << " at axis "
+             << quantizedDimension
+             << " must be divisible by the corresponding block size "
+             << blockSize;
+    if (tensorType.isDynamicDim(quantizedDimension))
+      expectedScaleShape[quantizedDimension] = ShapedType::kDynamic;
+    else
+      expectedScaleShape[quantizedDimension] =
+          tensorType.getDimSize(quantizedDimension) / blockSize;
+  }
+
+  // Block sizes must be greater than 0 and divide the corresponding dimension
+  // size. While a block size b must be less than or equal to the corresponding
+  // dimension size d, this constraint is implicitly enforced by requiring that
+  // d % b == 0 when d != 0.
+  //
+  // However, a problem arises when d = 0.  The divisibility constraint allows b
+  // to be any value, potentially violating the requirement that b <= d.
+  // Furthermore, if b is unspecified (implicitly equal to d), it violates the
+  // constraint that b > 0.
+  //
+  // Therefore, we explicitly disallow the case where d = 0 to maintain
+  // consistency and avoid these issues.
+  if (llvm::find(tensorType.getShape(), 0) != tensorType.getShape().end()) {
+    return op->emitError() << "tensor dimension size of zero is not allowed "
+                              "with sub-channel quantization";
+  }
+
+  auto scaleShape =
+      uniformQuantizedSubChannelType.getScales().getType().getShape();
+  if (scaleShape.size() != shape.size()) {
+    return op->emitError() << "Rank of scales " << scaleShape.size()
+                           << " must match "
+                           << "the rank of the tensor " << shape.size();
+  }
+
+  for (auto [index, scaleDim] : llvm::enumerate(expectedScaleShape)) {
+    if (expectedScaleShape[index] != ShapedType::kDynamic &&
+        expectedScaleShape[index] != scaleShape[index])
+      return op->emitError() << "dimension size " << scaleDim
+                             << " of scales tensor at axis " << index
+                             << " should match (tensor dimension at axis / "
+                                "block sizes at axis) = "
+                             << expectedScaleShape[index];
+  }
 
   return success();
 }
@@ -80,8 +168,20 @@ LogicalResult verifyQuantizationOp(Operation *op, QuantizedType quantizedType,
     return op->emitError(
         "expressed type in quantized type expected to match float type");
 
-  // Veriy integrity of per-axis quantization information, if present.
-  return verifyPerAxisQuantization(op, quantizedType, containerType);
+  // Verify integrity of per-axis quantization information, if present.
+  if (auto quantizedPerAxisType =
+          dyn_cast<UniformQuantizedPerAxisType>(quantizedType)) {
+    return verifyPerAxisQuantization(op, quantizedPerAxisType, containerType);
+  }
+
+  if (auto quantizedSubChannelType =
+          dyn_cast<UniformQuantizedSubChannelType>(quantizedType)) {
+    return verifySubChannelQuantization(op, quantizedSubChannelType,
+                                        containerType);
+  }
+
+  // At this point the type is UniformQuantizedType
+  return success();
 }
 
 }  // namespace
@@ -94,7 +194,7 @@ LogicalResult verifyQuantizationOp(Operation *op, QuantizedType quantizedType,
 void QuantDialect::initialize() {
   addTypes<AnyQuantizedType, CalibratedQuantizedType, UniformQuantizedType,
            UniformQuantizedPerAxisType, QuantileQuantizedType,
-           QuantileQuantizedPerAxisType>();
+           QuantileQuantizedPerAxisType, UniformQuantizedSubChannelType>();
   addOperations<
 #define GET_OP_LIST
 #include "mlir/Dialect/Quant/IR/QuantOps.cpp.inc"
@@ -176,7 +276,20 @@ LogicalResult StorageCastOp::verify() {
   // Verify integrity of per-axis quantization information, if available. While
   // the quantization type may appear in the input or the result, their tensor
   // shapes are guaranteed to be identical at this point.
-  return verifyPerAxisQuantization(*this, quantizedType, getInput().getType());
+  if (auto quantizedPerAxisType =
+          dyn_cast<UniformQuantizedPerAxisType>(quantizedType)) {
+    return verifyPerAxisQuantization(*this, quantizedPerAxisType,
+                                     getInput().getType());
+  }
+
+  if (auto quantizedSunChannelType =
+          dyn_cast<UniformQuantizedSubChannelType>(quantizedType)) {
+    return verifySubChannelQuantization(*this, quantizedSunChannelType,
+                                        getInput().getType());
+  }
+
+  // At this point the type is UniformQuantizedType
+  return success();
 }
 
 OpFoldResult StorageCastOp::fold(FoldAdaptor adaptor) {