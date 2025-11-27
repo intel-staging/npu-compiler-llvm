intel/npu-plugin-llvm

Add method getInlineBlockAndPoint for DialectInlinerInterface

mlir/lib/Transforms/Inliner.cpp
@@ -565,7 +565,8 @@ static LogicalResult inlineCallsInSCC(Inliner &inliner, CGUseList &useList,
     useList.mergeUsesAfterInlining(it.targetNode, it.sourceNode);
 
     // then erase the call.
-    call.erase();
+    const auto *callInterface = inliner.getInterfaceFor(call->getDialect());
+    callInterface->eraseCall(call);
 
     // If we inlined in place, mark the node for deletion.
     if (inlineInPlace) {

intel-staging/npu-compiler-llvm

mlir/lib/Transforms/Utils/Inliner.cpp
@@ -687,7 +687,8 @@ Inliner::Impl::inlineCallsInSCC(InlinerInterfaceImpl &inlinerIface,
     useList.mergeUsesAfterInlining(it.targetNode, it.sourceNode);
 
     // then erase the call.
-    call.erase();
+    const auto *callInterface = inlinerIface.getInterfaceFor(call->getDialect());
+    callInterface->eraseCall(call);
 
     // If we inlined in place, mark the node for deletion.
     if (inlineInPlace) {